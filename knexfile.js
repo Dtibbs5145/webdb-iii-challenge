f9103e26e02e9f3b1a37b168330074d110fa7121
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-06-05 16:48:48 -0400

    I filled in the seeds, and index files

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/lambda.db3'
     },
     useNullAsDefault: true,
     debug: true
   }
-}
+};
